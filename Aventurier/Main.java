65027e535c5f2688ff5f94d21130c2a247f103a9
Yannis699 <yannis.cristini@gmail.com>
2022-06-25 19:54:58 +0200

    Displaying the map

--- Aventurier/Main.java
+++ Aventurier/Main.java
@@ -1,8 +1,40 @@
 package Aventurier;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
-	
+	public static void main(String[] args) {
 
+		// Chemin du fichier
+		String path = "/home/cristini/Bureau/carte.txt";
+		Scanner scanner = new Scanner(System.in);
+		// Chargement du fichier txt
+		// Le fichier d'entrée
+		File file = new File(path);
+		// Créer l'objet File Reader
+		try {
+			FileReader filereader = new FileReader(file);
+
+			// Créer l'objet BufferedReader
+			BufferedReader bufferedReader = new BufferedReader(filereader);
+			StringBuffer stringBuffer = new StringBuffer();
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				// ajoute la ligne au buffer
+				stringBuffer.append(line);
+				stringBuffer.append("\n");
+			}
+
+			System.out.println(stringBuffer);
+
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
 }
